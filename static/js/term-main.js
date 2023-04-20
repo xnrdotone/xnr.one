44a79802a79cd67fbd4b3f93f0eac1aed56aa2c3
xnrdotone <xnrdotone@gmail.com>
2023-04-21 02:01:51 +0300

    update

--- static/js/term-main.js
+++ static/js/term-main.js
@@ -29,7 +29,7 @@ var main = (function () {
             // sudo_help: "Execute a command as the superuser.",
             welcome: "Welcome to xnr.one! This is the insite terminal, you can use this to quickly navigate around the site! Press the tab key to list available commands.",
             internet_explorer_warning: "NOTE: I see you're using internet explorer, this website won't work properly.",
-            welcome_file_name: "welcome_message.txt",
+            welcome_file_name: "welcome_message",
             invalid_command_message: "<value>: command not found.",
             // reboot_message: "Preparing to reboot...\n\n3...\n\n2...\n\n1...\n\nRebooting...\n\n",
             permission_denied_message: "Unable to '<value>', permission denied.",
@@ -64,11 +64,8 @@ var main = (function () {
             }
         };
         Singleton.defaultOptions = {
-            "about.txt": "This website was made using only pure JavaScript with no extra libraries.\nI made it dynamic so anyone can use it, just download it from GitHub and change the config text according to your needs.\nIf you manage to find any bugs or security issues feel free to email me: luisbraganca@protonmail.com",
-            "getting_started.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
-            "contact.txt": "mail@example.com",
-            "social_network_1.txt": "https://www.socialite.com/username/",
-            "social_network_2.txt": "https://example.com/profile/9382/",
+            "about": "Welcome to xnr.one interactive terminal! The terminal is under heavy development at the moment and is not really functional. My aim is to make the whole site navigatable from the terminal unfortunalelly I am no javascript wizard...",
+            "contact": "xnrdotone@gmail.com",
         };
         return {
             getInstance: function (options) {
